d737798ca8612eb2ed50e50416379825ff3aebb2
Bobybuu <c4305772@gmail.com>
2023-09-09 05:08:28 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,13 +7,13 @@
  *
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
 	int i;
 
 	for (i = 97; i < 123; i++)
 	{
-		if(i != 101 && i != 113)
+		if (i != 101 && i != 113)
 		{
 			putchar(i);
 		}
