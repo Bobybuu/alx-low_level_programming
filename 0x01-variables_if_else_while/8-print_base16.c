7753c5931a41af590a59e05070105ab38cd006dd
Bobybuu <c4305772@gmail.com>
2023-09-09 05:54:30 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,13 +8,14 @@
 int main(void)
 {
 	int i;
+
 	for(i = 48; i < 58; i++)
 	{
-		putchar(i);
+		putchar (i);
 	}
 	for(i = 97; i < 103; i++)
 	{
-		putchar(i);
+		putchar (i);
 	}
 	putchar('\n');
 	return (0);
