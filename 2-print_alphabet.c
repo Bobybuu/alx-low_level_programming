383ccb14f4c1cf7fdbf3c87a98d0ed31b090621f
Bobybuu <c43057772@gmail.com>
2025-03-16 00:45:22 +0300

    alphabets printing

--- 2-print_alphabet.c
+++ 2-print_alphabet.c
@@ -1,17 +1,13 @@
 #include <stdio.h>
 /**
  * main - A program that prints the alphabet in lowercase
- * using putchar
- * followed by a new line
  * Return: 0 (success)
  */
 int main(void)
 {
 char letter;
 for (letter = 'a'; letter <= 'z'; letter++)
-{
 putchar(letter);
-}
 putchar("\n");
 return (0);
 }
