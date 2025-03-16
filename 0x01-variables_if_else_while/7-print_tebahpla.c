c269826e08d8a32ca4cf13ff5a7214cd1dcca861
Bobybuu <c43057772@gmail.com>
2025-03-16 03:30:21 +0300

    A program that prints the alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - A program that prints the alphabet in reverse
+ * Return: 0 (success)
+ */
+int main(void)
+{
+char letter;
+for (letter = 'z'; letter >= 'a'; letter--)
+    {
+    putchar(letter);
+    }
+    return (0);
+}
+
