55f973e2470139e4711ab9d5bf8bb2c88d101e99
Bobybuu <c43057772@gmail.com>
2025-03-18 02:58:06 +0300

    changes

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /** 
-*void print_alphabet_x10-prints alphabets 10X
+*void print_alphabet_x10-prints 
+*alphabets 10X now
 *Return: 0 (success)
 */
 void print_alphabet_x10(void)
@@ -14,7 +15,7 @@ while(letter <= 'z')
 _putchar(letter);
 letter++;
 }
+_putchar('\n');
 i++;
 }
-_putchar('\n');
 }
