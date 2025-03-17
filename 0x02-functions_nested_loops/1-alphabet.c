d16f6b15a922b3d23bdd0e77ea7fc424a12ac515
Bobybuu <c43057772@gmail.com>
2025-03-18 02:06:06 +0300

    A program that prints alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main-A program that prints alphabets
+ * Return: 0 (success)
+ */
+ void print_alphabet(void)
+ {
+    char letter = 'a';
+    while(letter <= 'z')
+    {
+        _putchar(letter);
+        letter++;
+    }
+ }
