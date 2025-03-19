72c8ce826074a59b4777c7c17a14c6095592c5a8
Bobybuu <c43057772@gmail.com>
2025-03-19 18:50:01 +0300

    a program that prints lower alphabets

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  *_islower-a program that prints lower alphabets
- *@c- character to be checked
- *Return 0 (success)
+ *@c: character to be checked
+ *Return: 1 if c is lowercase, 0 otherwise
  */
 int _islower(int c)
 {
