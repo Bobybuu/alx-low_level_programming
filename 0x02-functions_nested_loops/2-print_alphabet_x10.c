e6b6b1803843a428b0f44ddfa5b113ea0ac95ce2
Bobybuu <c43057772@gmail.com>
2025-03-19 18:04:55 +0300

    program prints alphabets 10X

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,9 +9,10 @@ int i;
 char letter;
 for (i = 1; i <= 10; i++)
 {
-for (letter = 'a'; letter <= 'z'; letter++ )
+for (letter = 'a'; letter <= 'z'; letter++)
 {
 _putchar(letter);
+_putchar('\n');
 }
 }
 }
