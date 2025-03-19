53396a1238c7ee839a664674f7d8e5eb397b3209
Bobybuu <c43057772@gmail.com>
2025-03-19 18:42:17 +0300

    a program that prints lower alphabets

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,11 +1,16 @@
 #include "main.h"
 /**
  *int _islower-a program that prints lower alphabets
- *Return: 0 (success)
+ *Return 0 (success)
  */
 int _islower(int c)
 {
-char letter;
-letter = 'c', 'C';
-x = letter.isalpha()
+if ( c >= 'a' && c <= 'z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
 }
