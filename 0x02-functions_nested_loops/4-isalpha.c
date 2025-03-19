cc983c2c71378e8bb1813c7d678ec85d179f89a8
Bobybuu <c43057772@gmail.com>
2025-03-19 19:36:51 +0300

    function that checks for alphabetic character.Returns 1 if c is a letter, lowercase or uppercase

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- *_islower-a program that prints alphabets
+ *_islower-checks for alphabetic character
  *@c: character to be checked
  *Return: 1 if c is lowercase, 0 otherwise
  */
@@ -12,5 +12,6 @@ return (1);
 }
 else
 {
+return (0);
 }
 }
