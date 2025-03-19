d948876f683117df4057534690833371f978f7b6
Bobybuu <c43057772@gmail.com>
2025-03-19 19:42:25 +0300

    function that checks for alphabetic character.Returns 1 if c is a letter, lowercase or uppercase

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,5 +6,12 @@
  */
 int _islower(int c)
 {
-return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+{
+return (1);
+}
+else
+{
+return (0);
+}
 }
