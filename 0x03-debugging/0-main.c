47e51faaf3181169ef793bd1a58f3c91e827ac97
Bobybuu <c43057772@gmail.com>
2025-05-20 21:58:35 +0300

    Docs: writing a test

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - tests function that check if interger is positive or negative
+ * Return: 0
+ */
+int main()
+{
+    int i;
+    i = 0;
+    void positive_or_negative(int i);
+    return (0);
+}
