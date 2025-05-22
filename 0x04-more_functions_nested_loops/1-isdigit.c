a40b7d0a46b86d1388d7688e812d7f75b1f9db71
Chrispin Odiwuor <C43057772@gmail.com>
2025-05-22 04:11:50 -0700

    Docs: a function that checks for a digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+/**
+ *_isdigit - a function that checks for a digit
+ *@c: character to check
+ *Return: 1 if c is a digit or 0 otherwise
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+
+		return (0);
+	}
+}
+
